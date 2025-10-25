01e79e87b926f794d49878f63b9cec9b29d063e6
Inbal <inbaltsa@post.bgu.ac.il>
2025-10-25 16:14:24 +0300

    תיקון הערה-הוכחה-פתרון

--- env-to-callout.lua
+++ env-to-callout.lua
@@ -1,17 +1,15 @@
--- env-to-callout.lua
 -- Map LaTeX environments \begin{solution}...\end{solution} and \begin{hint}...\end{hint}
 -- to Quarto callouts when rendering HTML. No effect in PDF.
 function Div(div)
   if not quarto.doc.isFormat("pdf") then
     if div.classes:includes("solution") then
-      -- Turn into a callout-note that starts collapsed
-      div.classes = pandoc.List({"callout", "callout-note"})
+      -- קובעים מחלקות נקיות: בלי proof
+      div.classes = pandoc.List({ "callout", "callout-note", "solution" })
       div.attributes["collapse"] = "true"
       div.attributes["title"] = "הצג פתרון"
       return div
     elseif div.classes:includes("hint") then
-      -- Turn into a callout-tip that starts collapsed
-      div.classes = pandoc.List({"callout", "callout-tip"})
+      div.classes = pandoc.List({ "callout", "callout-tip", "hint" })
       div.attributes["collapse"] = "true"
       div.attributes["title"] = "רמז"
       return div
